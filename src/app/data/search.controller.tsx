13373d6fffb5ec5a385499792a85fca4dc1f5c5f
Gaerax <gaerax@gmail.com>
2023-03-02 18:47:28 -0800

    removed passwords, etc from search

--- src/app/data/search.controller.tsx
+++ src/app/data/search.controller.tsx
@@ -1,11 +1,11 @@
 import { client, index, type } from './connection';
-import { SearchParams } from '../../../spriggan-shared/types/SearchTypes';
+import { SearchParams, InstallDataParams } from '../../../spriggan-shared/types/SearchTypes';
 
 import Koa from 'koa';
 import Router from 'koa-router';
 
 const routerOpts: Router.IRouterOptions = {
-	prefix: '/games',
+	prefix: '/media',
 };
 
 const router: Router = new Router(routerOpts);
@@ -20,14 +20,17 @@ router.get('/search', async (ctx: Koa.Context) => {
 router.get('/mostRecent', async (ctx: Koa.Context) => {
 		const searchParams = ctx.request.query as unknown as SearchParams;
 		console.log('Search params: ', searchParams);
-		ctx.body = await search.mostRecent(searchParams);
+		const res = await search.mostRecent(searchParams);
+		console.log(res)
+		ctx.body = res;
 	},
 );
 
-router.get('/installData', async (ctx: Koa.Context) => {
-		const searchParams = ctx.request.query as unknown as SearchParams;
+router.get('/getInstallData', async (ctx: Koa.Context) => {
+		const searchParams = ctx.request.query as unknown as InstallDataParams;
 		console.log('Search params: ', searchParams);
-		ctx.body = await search.mostRecent(searchParams);
+		const res = await search.installData(searchParams);
+		ctx.body = res;
 	},
 );
 
@@ -36,6 +39,9 @@ const search = {
 		return client.search({
 			from: params.offset,
 			index: index,
+			_source: {
+				"excludes": [ "*.password", "*.torrents", "*.executables" ]
+			},
 			query: {
 				bool: {
 					must: [
@@ -65,6 +71,28 @@ const search = {
 				bool: {
 					must: [
 						{match_all: { } },
+						{match: {
+							isPublic: {
+								query: true
+							}
+						}}
+					],
+				},
+			},
+			highlight: { fields: { title: {} } },
+		});
+	},
+	installData: (params: InstallDataParams) => {
+		return client.search({
+			index: index,
+			query: {
+				bool: {
+					must: [
+						{match: {
+							productId: {
+								query: params.productId
+							}
+						}},
 						{match: {
 							isPublic: {
 								query: true
